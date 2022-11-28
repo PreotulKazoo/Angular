aaba805d9fb25f4a021d8f3c60333b44fc1fec00
Mihaita <60981909+PreotulKazoo@users.noreply.github.com>
2022-11-28 10:35:22 +0200

    adding some code

--- test-app-js-master/public/app/admin/persons/personsCtrl.js
+++ test-app-js-master/public/app/admin/persons/personsCtrl.js
@@ -23,34 +23,34 @@ function personsCtrl($scope, $loading, $localStorage, $uibModal, dialogs, toastr
 		utils.setHeight('scroll', 40, 'maxHeight');
   });
 
-  // vm.add = () => {
-  //   $uibModal.open({
-  //     templateUrl: 'app/admin/information/informationModal/informationModal',
-  //     controller: 'informationModalCtrl',
-  //     controllerAs: 'mm',
-  //     size: 'sm',
-  //     resolve: {id_info: () => null}
-  //   }).result.then(() => load()).catch(() => null);
-  // };
+  vm.add = () => {
+    $uibModal.open({
+      templateUrl: 'app/admin/persons/personsModal/personsModal',
+      controller: 'personsModalCtrl',
+      controllerAs: 'mm',
+      size: 'sm',
+      resolve: {id_info: () => null}
+    }).result.then(() => load()).catch(() => null);
+  };
 
-  // vm.edit = id => {
-  //   $uibModal.open({
-  //     templateUrl: 'app/admin/information/informationModal/informationModal',
-  //     controller: 'informationModalCtrl',
-  //     controllerAs: 'mm',
-  //     size: 'sm',
-  //     resolve: {id_info: () => id}
-  //   }).result.then(()=>load()).catch(()=>null);
-  // };
+  vm.edit = id => {
+    $uibModal.open({
+      templateUrl: 'app/admin/persons/personsModal/personsModal',
+      controller: 'personsModalCtrl',
+      controllerAs: 'mm',
+      size: 'sm',
+      resolve: {id_info: () => id}
+    }).result.then(()=>load()).catch(()=>null);
+  };
 
-  // vm.remove = ob => {
-  //   dialogs.confirm(`Confirmă ștergerea`, `Doriți să ștergeți rândul?`).result.then(() => {
-  //     $loading.start(`loading-container`);
-  //     personsModel.byId.remove({id: ob.id}).$promise.then(() => {
-  //       $loading.finish(`loading-container`);
-  //       toastr.success(`Rândul a fost șters!`);
-  //       load();
-  //     }).catch(() => toastr.error(`Eroare la ștergere`));
-  //   }).catch(() => null);
-  // };
+  vm.remove = ob => {
+    dialogs.confirm(`Confirmă ștergerea`, `Doriți să ștergeți rândul?`).result.then(() => {
+      $loading.start(`loading-container`);
+      personsModel.byId.remove({id: ob.id}).$promise.then(() => {
+        $loading.finish(`loading-container`);
+        toastr.success(`Rândul a fost șters!`);
+        load();
+      }).catch(() => toastr.error(`Eroare la ștergere`));
+    }).catch(() => null);
+  };
 }
